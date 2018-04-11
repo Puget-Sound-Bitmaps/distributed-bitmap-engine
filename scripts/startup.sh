8a1214d75ef76ea9a5457c9ec7bfca70c817e347
Jahrme Risner <jrisner@pugetsound.edu>
2018-04-10 17:31:08 -0700

    Fixed (Maybe?)

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -6,24 +6,20 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
 # Rebuild and run necessary preparatory commands.
 sudo rpcbind
 
-cd $DIR/bitmap-engine
-make
-
 cd $DIR/distributed-system
-make
 
 # Fist argument is the type of node [master|slave]
 NODETYPE=$1
 
 # Assign binary file depending on argument, quitting if invalid.
-if [[ NODETYPE == "slave" ]]
+if [[ $NODETYPE == "slave" ]]
 then
     make spawn_slave
-elif [[ NODETYPE == "master" ]]
+elif [[ $NODETYPE == "master" ]]
 then
     make basic_test
 else
-    echo "`$1` is not a valid argument."
+    echo "`$NODETYPE` is not a valid argument."
     echo "Usage: startup.sh [master|slave]"
     exit 1
 fi
