9490534f2144c338912ab74d4c2e47b720fff269
Jahrme Risner <jrisner@pugetsound.edu>
2018-04-10 18:39:32 -0700

    Actually start

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -17,9 +17,13 @@ make
 if [[ $NODETYPE == "slave" ]]
 then
     echo "Starting slave..."
+    cd $DIR/distributed-system/bin
+    nohup ./slave &
 elif [[ $NODETYPE == "master" ]]
 then
     echo "Starting master..."
+    cd $DIR/distributed-system/bin
+    nohup ./dbms 0 &
 else
     echo "`$NODETYPE` is not a valid argument."
     echo "Usage: startup.sh [master|slave]"
