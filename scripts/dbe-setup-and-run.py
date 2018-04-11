c692ee0cdf2308f24d90d0c9a84f6e870069dfd8
Jahrme Risner <jrisner@pugetsound.edu>
2018-04-10 20:04:45 -0700

    It is Functional, Yay

--- scripts/dbe-setup-and-run.py
+++ scripts/dbe-setup-and-run.py
@@ -1,5 +1,4 @@
-import sys
-import paramiko
+import sys, os, string, threading, paramiko
 
 #########
 # SETUP #
@@ -70,10 +69,7 @@ for username, hostname in slaves:
         "sudo rpcbind",
 
         # Build system.
-        "cd {dir}/distributed-system && make".format(dir=(git_dir.format(username=username))),
-
-        # Start
-        "cd {dir}/distributed-system/bin && nohup ./slave &".format(dir=(git_dir.format(username=username)))
+        "cd {dir}/distributed-system && make".format(dir=(git_dir.format(username=username)))
     ]
 
     for command in slave_setup:
@@ -116,10 +112,7 @@ master_setup = [
     "sudo rpcbind",
 
     # Build System
-    "cd {dir}/distributed-system && make".format(dir=(git_dir.format(username=username))),
-
-    # Start
-    "cd {dir}/distributed-system/bin && nohup ./dbms 0".format(dir=(git_dir.format(username=username)))
+    "cd {dir}/distributed-system && make".format(dir=(git_dir.format(username=username)))
 ]
 
 for command in master_setup:
@@ -133,3 +126,42 @@ for command in master_setup:
         print("Err:\n{}".format(err))
 
 ssh.close()
+
+
+print("\n" + ("-"*80) +  "STARTING SYSTEMS" + ("-"*80) + "\n")
+
+outlock = threading.Lock()
+start_cmd = "cd {dir}/distributed-system/bin && ./{type}"
+
+def start(hostname, username, kind):
+    sshT = paramiko.SSHClient()
+    sshT.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    sshT.connect(hostname=hostname, username=username, pkey=pkey)
+
+    if kind == "master":
+        kind = "dbms 0"
+
+    stdin, stdout, stderr = sshT.exec_command(start_cmd.format(dir=(git_dir.format(username=username)), type=kind))
+
+    out = stdout.read().decode()
+    err = stderr.read().decode()
+    if len(out) > 0:
+        print("Out:\n{}".format(out))
+    if len(err) > 0:
+        print("Err:\n{}".format(err))
+
+
+threads = []
+
+for username, hostname in slaves:
+    t = threading.Thread(target=start, args=(hostname, username, "slave"))
+    t.start()
+    threads.append(t)
+
+for username, hostname in [servers.master_node]:
+    t = threading.Thread(target=start, args=(hostname, username, "master"))
+    t.start()
+    threads.append(t)
+
+for t in threads:
+    t.join()
